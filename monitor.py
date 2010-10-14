23830f8b0ff834821115a408f90679fb78682b43
Your Name <you@example.com>
2010-10-14 19:42:39 +0000

    Starting to work on the XML procesing.

--- monitor.py
+++ monitor.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
 import socket
+import xml.dom
 from pygphone import GooglePhone, GoogleSMS
 
-servers = [ 'devio.us' ]
-ports   = [ 22 ]
 
-alert_list = {  'daniel':'', 'jared':'', 'cmeiklejohn':'', 'robin':'', 
-		'kyle':'+17202515371', 'bryce':'' }
 
 
