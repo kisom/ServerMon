6a96e552e66ef9a80683b4ce33e04f471e026b26
Kyle Isom <kisom@debian.(none)>
2010-10-14 15:15:30 -0400

    Initial commit.

---
+++ monitor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import socket
+from pygphone import GooglePhone, GoogleSMS
+
+servers = [ 'devio.us' ]
+ports   = [ 22 ]
+
+alert_list = {  'daniel':'', 'jared':'', 'cmeiklejohn':'', 'robin':'', 
+		'kyle':'+17202515371', 'bryce':'' }
+
+
